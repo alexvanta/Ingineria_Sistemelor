95024dff4afff179909c4fc540110451f9bcff6d
Vanta Alexandru-Mihai <alexandrumihai.vanta@ulbsibiu.ro>
2026-03-23 20:12:15 +0200

    problema 3.5.2

---
+++ src/lab3/probl352.java
@@ -0,0 +1,61 @@
+package lab3;
+import lab2.Student;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class probl352 {
+    public static void main(String[] args){
+
+            Path inputPath = Paths.get("studenti_in.txt");
+            Path outputPath = Paths.get("studenti_out.txt");
+            try{
+
+            List<String> linii = Files.readAllLines(inputPath);
+            List<Student> listaStudenti = new ArrayList<>();
+            for(String linie : linii){
+                if(linie.trim().isEmpty()) continue;
+                String[] parti = linie.split(",");
+                if(parti.length == 4) {
+                    try{
+                        int nrMatricol = Integer.parseInt(parti[0].trim());
+                        String prenume = parti[1].trim();
+                        String nume = parti[2].trim();
+                        String formatieDeStudiu = parti[3].trim();
+                        listaStudenti.add(new Student(nrMatricol, prenume, nume, formatieDeStudiu));
+                    }
+                    catch (NumberFormatException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+            for(Student s : listaStudenti){
+                System.out.println(s);
+            }
+            Collections.sort(listaStudenti, new Comparator<Student>() {
+                @Override
+                        public int compare(Student s1, Student s2){
+                    return s1.getNume().compareToIgnoreCase(s2.getNume());
+                }
+            });
+            List<String> liniiSortate = new ArrayList<>();
+            liniiSortate.add("Studenti sortati dupa nume: ");
+            for(Student s : listaStudenti){
+                liniiSortate.add(s.toString());
+            }
+            Files.write(outputPath,liniiSortate);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        catch(NumberFormatException e){
+            e.printStackTrace();
+        }
+    }
+}
