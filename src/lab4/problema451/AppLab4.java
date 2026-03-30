01382e80ea0253464b22b840c119fe2d15cc90f0
Vanta Alexandru-Mihai <alexandrumihai.vanta@ulbsibiu.ro>
2026-03-31 00:20:48 +0300

    problema 451

--- src/lab4/problema451/AppLab4.java
+++ src/lab4/problema451/AppLab4.java
@@ -1,23 +1,6 @@
-package lab4;
-
+package lab4.problema451;
 import java.util.HashMap;
 import java.util.Map;
-
- class Tanar {
-    private String name;
-    private int age;
-    private String address;
-
-    public Tanar(String name, int age, String address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
-    @Override
-     public String toString() {
-         return "Tanar {nume = '" + name + "', varsta = " + age + " , adresa = '" + address + "'}";
-    }
-}
 
 public class AppLab4 {
     static void main() {
