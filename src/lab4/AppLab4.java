3aef28bd1f381176c507b39789ad8925272c48b6
Vanta Alexandru-Mihai <alexandrumihai.vanta@ulbsibiu.ro>
2026-03-30 22:27:58 +0300

    Problema 4.5.1

---
+++ src/lab4/AppLab4.java
@@ -0,0 +1,52 @@
+package lab4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+ class Tanar {
+    private String name;
+    private int age;
+    private String address;
+
+    public Tanar(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    @Override
+     public String toString() {
+         return "Tanar {nume = '" + name + "', varsta = " + age + " , adresa = '" + address + "'}";
+    }
+}
+
+public class AppLab4 {
+    static void main() {
+        HashMap<String, Integer> varste = new HashMap<>();
+        varste.put("Ioan", 21);
+        varste.put("Maria", 22);
+        varste.put("Victor", 20);
+        varste.put("Simina", 20);
+        varste.put("Marius", 21);
+        varste.put("Mihai", 21);
+        varste.put("Daniela", 23);
+
+        varste.put("Vlad", 19);
+        varste.put("Iulia", 19);
+
+        Map<String, String> adrese = Map.of("Ioan", "Sibiu", "Maria", "Bucuresti", "Victor",
+                "Cluj", "Simina", "Alba-Iulia", "Marius", "Medias", "Mihai", "Cisnadie", "Daniela", "Sibiu");
+
+        System.out.println("Continutul instantei varste este: " + varste);
+
+        HashMap<String, Tanar> tineri = new HashMap<>();
+        for (String nume : varste.keySet()) {
+            int varsta = varste.get(nume);
+            String adresa = adrese.getOrDefault(nume, "Adresa necunoscuta");
+            tineri.put(nume, new Tanar(nume, varsta, adresa));
+        }
+        System.out.println("Continutul instantei tineri este: ");
+        for(Tanar t : tineri.values()) {
+            System.out.println(t);
+        }
+    }
+}
