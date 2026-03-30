cc30f4f13049663a044a71d30308da80682f7462
Vanta Alexandru-Mihai <alexandrumihai.vanta@ulbsibiu.ro>
2026-03-31 00:22:22 +0300

    problema 452

---
+++ src/lab4/problema452/Student.java
@@ -0,0 +1,39 @@
+package lab4.problema452;
+
+import java.util.Objects;
+
+public class Student {
+    private int nrMatricol;
+    private String prenume;
+    private String nume;
+    private String formatieDeStudiu;
+    private float nota;
+
+    public Student(int nrMatricol, String prenume, String nume, String formatieDeStudiu){
+        this.nrMatricol = nrMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+    public void setNota(float nota){
+        this.nota = nota;
+    }
+    public int getNrMatricol(){
+        return nrMatricol;
+    }
+    @Override
+    public String toString(){
+        return "Student{" + "nrMatricol = " + nrMatricol + ", nume = '" + nume + '\'' + ", nota = " + nota + '}';
+    }
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return nrMatricol == student.nrMatricol;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(nrMatricol);
+    }
+}
